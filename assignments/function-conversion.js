adca25c52c74b60dd4c6158f97fc40bcdef7b20f
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-07-22 17:21:18 -0500

    Completed function.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -9,7 +9,7 @@ console.log(anotherFunction("Example"));
 let add = (param1, param2) => {return param1 + param2;};
 add(1,2);
 
-let subtract = (param1, param2) => {return param1 - param2};
+let subtract = (param1, param2) => {return param1 - param2;};
 console.log(subtract(1,2));
 
 
